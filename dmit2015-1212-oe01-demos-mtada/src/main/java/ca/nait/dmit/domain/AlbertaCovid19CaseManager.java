c90c8e9d3803879237cdae00a18d7b638d814aba
Midori Tada` <mtada1@nait.ca>
2022-01-19 16:33:36 -0700

    COVID data and Java stream demo

--- dmit2015-1212-oe01-demos-mtada/src/main/java/ca/nait/dmit/domain/AlbertaCovid19CaseManager.java
+++ dmit2015-1212-oe01-demos-mtada/src/main/java/ca/nait/dmit/domain/AlbertaCovid19CaseManager.java
@@ -9,17 +9,32 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
 public class AlbertaCovid19CaseManager {
+    private static AlbertaCovid19CaseManager instance;
+    public static AlbertaCovid19CaseManager getInstance() throws IOException {
+        // https://www.journaldev.com/1377/java-singleton-design-pattern-best-practices-examples#thread-safe-singleton
+        if(instance == null){
+            synchronized (AlbertaCovid19CaseManager.class) {
+                if(instance == null){
+                    instance = new AlbertaCovid19CaseManager();
+                }
+            }
+        }
+        return instance;
+    }
     @Getter
     private List<AlbertaCovid19Case> albertaCovid19CaseList = new ArrayList<>();
-    public AlbertaCovid19CaseManager() throws IOException {
-        try(var reader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream(  "/data/covid-19-alberta-statistics-data.csv")))) {
-            String lineText;  // Declare a delimiter that looks for a comma inside a value
-            final var DELIMITER = ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";  // We can skip the first line since its contains header columns
+    private AlbertaCovid19CaseManager() throws IOException {
+        try(var reader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream(
+                "/data/covid-19-alberta-statistics-data.csv")))) {
+            String lineText;
+            // Declare a delimiter that looks for a comma inside a value
+            final var DELIMITER = ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
+            // We can skip the first line since its contains header columns
             reader.readLine();
             var dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-            while ( (lineText = reader.readLine()) != null) {  // Create an object for each row in the file
+            while ( (lineText = reader.readLine()) != null) {
+                // Create an object for each row in the file
                 AlbertaCovid19Case currentCase = new AlbertaCovid19Case();
                 String[] values = lineText.split(DELIMITER, -1); // The -1 limit allows for any number of fields and not discard empty fields
                 // Column order of fields:
@@ -44,10 +59,40 @@ public class AlbertaCovid19CaseManager {
     public List<String> findDistinctAhsZone() {
         return albertaCovid19CaseList
                 .stream()
-                .map(AlbertaCovid19Case::getAhsZone)
+//                .map(AlbertaCovid19Case::getAhsZone)
+                .map(item -> item.getAhsZone())
                 .distinct()
+                .filter(item -> item.isEmpty() == false)
                 .sorted()
                 .collect(Collectors.toList());
     }
+
+    public long activeCaseCount() {
+        return albertaCovid19CaseList
+                .stream()
+                .filter(item -> item.getCaseStatus().equalsIgnoreCase("Active"))
+                .count();
+    }
+
+    public long activeCaseCountByAhsZone(String ahsZone) {
+        return albertaCovid19CaseList
+                .stream()
+//                .filter(item -> item.getCaseStatus().equalsIgnoreCase("Active")
+//                    && item.getAhsZone().equalsIgnoreCase(ahsZone));
+                .filter(item -> item.getCaseStatus().equalsIgnoreCase("Active"))
+                .filter(item -> item.getAhsZone().equalsIgnoreCase(ahsZone))
+                .count();
+    }
+
+    public long caseReportedCountByAhsZoneAndDateRange(String ahsZone, LocalDate fromDate, LocalDate toDate) {
+        return albertaCovid19CaseList
+                .stream()
+                .filter(item -> item.getAhsZone().equalsIgnoreCase(ahsZone))
+                .filter(item -> !item.getDateReported().isBefore(fromDate) && !item.getDateReported().isAfter(toDate))
+                .count();
+    }
 }
+
+
+
 
--- dmit2015-1212-oe01-demos-mtada/src/test/java/ca/nait/dmit/domain/AlbertaCovid19CaseManagerTest.java
+++ dmit2015-1212-oe01-demos-mtada/src/test/java/ca/nait/dmit/domain/AlbertaCovid19CaseManagerTest.java
@@ -1,29 +1,78 @@
 package ca.nait.dmit.domain;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AlbertaCovid19CaseManagerTest {
 
-    private static AlbertaCovid19CaseManager caseManager;
+//    private static AlbertaCovid19CaseManager caseManager;
+//
+//    @BeforeAll
+//    static void beforeAll() throws IOException {
+//        caseManager = new AlbertaCovid19CaseManager();
+//    }
 
-    @BeforeAll
-    static void beforeAll() throws IOException {
-        caseManager = new AlbertaCovid19CaseManager();
+    AlbertaCovid19CaseManager caseManager;
+
+    @BeforeEach
+    void setUp() throws IOException {
+//        caseManager = new AlbertaCovid19CaseManager();
+        caseManager = AlbertaCovid19CaseManager.getInstance();
     }
 
     @Test
     void getAlbertaCovid19CaseList() {
-        assertEquals(436495, caseManager.getAlbertaCovid19CaseList().size());
+        assertEquals(452381, caseManager.getAlbertaCovid19CaseList().size());
     }
     
     @Test
     void distinctAhsZone() {
-        caseManager.findDistinctAhsZone().forEach(System.out::println);
-        assertEquals(7, caseManager.findDistinctAhsZone().size());
+//        caseManager.findDistinctAhsZone().forEach(System.out::println);
+        List<String> ahsZoneList = caseManager.findDistinctAhsZone();
+
+        // The next two lines are the same
+//        ahsZoneList.stream().forEach(System.out::println);
+        ahsZoneList.stream().forEach(item -> System.out.println(item));
+
+        assertEquals(6, caseManager.findDistinctAhsZone().size());
+    }
+
+    @Test
+    void activeCaseCount() {
+        assertEquals(72_368, caseManager.activeCaseCount());
+    }
+
+    @Test
+    void activeCaseCountByZone() {
+        assertEquals(26_002, caseManager.activeCaseCountByAhsZone("Edmonton Zone"));
+        assertEquals(29_544, caseManager.activeCaseCountByAhsZone("Calgary Zone"));
+    }
+
+    @Test
+    void caseReportedCountByAhsZoneAndDateRange() {
+        assertEquals(15_699, caseManager.caseReportedCountByAhsZoneAndDateRange("Edmonton Zone", LocalDate.parse("2022-01-07"), LocalDate.parse("2022-01-13")));
+        assertEquals(19_434, caseManager.caseReportedCountByAhsZoneAndDateRange("Calgary Zone", LocalDate.parse("2022-01-07"), LocalDate.parse("2022-01-13")));
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
